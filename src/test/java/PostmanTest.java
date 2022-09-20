392d331e665c6059f882474032de6146e7ba5c20
Varek1807 <105634176+Varek1807@users.noreply.github.com>
2022-09-20 22:09:37 +0500

    Update PostmanTest.java

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -11,7 +11,7 @@ public class PostmanTest {
         // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("data") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
